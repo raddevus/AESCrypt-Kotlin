66580834697b60470d720c073e16b4779e8dd495
raddevus <1radeutsch1@gmail.com>
2021-12-18 16:56:58 -0500

    Decryption works exactly as it does in the Node app & the dotnet app.  You can set a password using a typed string and set your cleartext you want to encrypt, push the button and it encrypts and encodes the data into base64 data that matches node & dotnet projects.

--- app/src/main/java/com/newlibre/aescrypt/MainActivity.kt
+++ app/src/main/java/com/newlibre/aescrypt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.newlibre.aescrypt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import java.lang.Exception
 import java.nio.charset.Charset
@@ -22,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var encryptButton : Button
     lateinit var decryptButton : Button
+    lateinit var passwordText : EditText
+    lateinit var clearText : EditText
 
     lateinit var mainText : TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,48 +32,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         encryptButton = findViewById(R.id.encryptButton)
         mainText = findViewById(R.id.mainText)
+        passwordText = findViewById(R.id.passwordText)
+        clearText = findViewById(R.id.clearText)
         encryptButton.setOnClickListener {
-            mainText.setText(R.string.app_name)
+            mainText.setText(encryptData(clearText.text.toString().toByteArray(),
+                passwordText.text.toString()))
         }
         decryptButton = findViewById(R.id.decryptButton)
         decryptButton.setOnClickListener {
-            mainText.setText(encryptData())
+            mainText.setText(R.string.app_name)
         }
+
+
+
     }
 
-    fun encryptData() : String{
-        val plaintext: ByteArray = "abc".toByteArray(StandardCharsets.UTF_8);
+    fun encryptData(plainText : ByteArray, password: String) : String{
 
         val keygen = KeyGenerator.getInstance("AES")
         keygen.init(256)
         val key : SecretKey = keygen.generateKey()
-        val password = "ab";
+
+        // when you convert any string to a Sha256 it will always be 32 bytes (256 bits)
+        // which is exactly the size we need our AES key to be.
         val rawSha256OfPassword = ConvertStringToSha256(password);
         var keyAndIV= BytesToHex(ConvertStringToSha256(password))
 
         //val spec1 = SecretKeySpec(keyAndIV.substring(0..31).toByteArray(StandardCharsets.UTF_8), "AES")
         val spec1 = SecretKeySpec(rawSha256OfPassword, "AES")
-        //val spec = PBEKeySpec( "a".toCharArray(),"a".toByteArray(StandardCharsets.UTF_8), 1)//,1)// 128 * 8)
 
-            // val key2: SecretKey = SecretKeyFactory.getInstance("PBEwithSHA256AND256BITAES-CBC-BC").generateSecret(spec)//"PBEwithSHA256AND256BITAES-CBC-BC").generateSecret(spec)
-            //.getInstance("PBEwithHmacSHA256AndAES_128").generateSecret(spec)
-
-        //key2.
         // Toast.makeText(applicationContext,key.toString(),Toast.LENGTH_LONG).show()
         //val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")//"AES/CBC/PKCS5PADDING")
         //cipher.init(Cipher.ENCRYPT_MODE, key2)
 
 //        val ciphertext: ByteArray = cipher.doFinal(plaintext)
 //        val key3: SecretKey = keygen.generateKey()
-        val myIV : ByteArray = rawSha256OfPassword.slice(0..15).toByteArray()//keyAndIV.substring(0..15).toByteArray(StandardCharsets.UTF_8) // ALWAYS 16 bytes!!
-        val newCipherText : ByteArray = encrypt("a".toByteArray(StandardCharsets.UTF_8),myIV,spec1)
-//        val ciphertext2: ByteArray = cipher.doFinal();
-//        ciphertext = ciphertext + "  #### " + ciphertext2
 
-        //val iv: ByteArray = cipher.iv
-        //return cipher.iv.toBase64()
+        // myIV is ALWAYS 16 bytes!!
+        val myIV : ByteArray = rawSha256OfPassword.slice(0..15).toByteArray()//keyAndIV.substring(0..15).toByteArray(StandardCharsets.UTF_8)
+        val newCipherText : ByteArray = encrypt(plainText,myIV,spec1)
+
         return newCipherText.toBase64()
     }
+
     fun ByteArray.toBase64(): String =
         String(Base64.getEncoder().encode(this))
 
