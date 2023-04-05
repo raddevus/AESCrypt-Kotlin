7911c78f33bf143fb62a546c221a4dea29d007d1
raddevus <1radeutsch1@gmail.com>
2023-04-05 18:51:33 -0400

    Working through various scenarios and making code work exactly as .net aes256 and javascript aes256.

--- app/src/main/java/com/newlibre/aescrypt/MainActivity.kt
+++ app/src/main/java/com/newlibre/aescrypt/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.newlibre.aescrypt
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import java.lang.Exception
 import java.util.*
 
@@ -15,9 +18,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var decryptButton : Button
     lateinit var passwordText : EditText
     lateinit var clearText : EditText
+    lateinit var ivText : EditText
     lateinit var crypton : Crypton
 
     lateinit var mainText : TextView;
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -25,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         mainText = findViewById(R.id.mainText)
         passwordText = findViewById(R.id.passwordText)
         clearText = findViewById(R.id.clearText)
+        ivText = findViewById(R.id.ivText)
         encryptButton.setOnClickListener {
             if (passwordText.text.toString().isEmpty()){
                 Toast.makeText(applicationContext, "Please add a password & try again.",Toast.LENGTH_LONG).show()
@@ -32,10 +38,12 @@ class MainActivity : AppCompatActivity() {
             else {
                 crypton = Crypton(passwordText.text.toString(),
                     clearText.text.toString().toByteArray())
+                var encryptedText = crypton.processData(ivText.text.toString())
                 mainText.setText(
                     // call with true or blank -- true is default value
-                    crypton.processData()
+                    encryptedText
                 )
+                Log.d("Crypton", encryptedText)
             }
         }
 
@@ -49,7 +57,9 @@ class MainActivity : AppCompatActivity() {
                     val cipherBytes = Base64.getDecoder().decode(mainText.text.toString())
                     crypton = Crypton(passwordText.text.toString(),
                             cipherBytes)
-                    mainText.setText(crypton.processData(false))
+                    var decryptedText = crypton.processData(ivText.text.toString(),false)
+                    mainText.setText(decryptedText)
+                    Log.d("Crypton", decryptedText)
                 } catch (ex: Exception) {
                     Toast.makeText(
                         applicationContext,
@@ -62,6 +72,9 @@ class MainActivity : AppCompatActivity() {
             //Toast.makeText(applicationContext, cipherBytes.size.toString(),Toast.LENGTH_LONG).show()
 
         }
+        passwordText.setText("c4747607e721580882e7186c136b22d9670779af296772a7abb76f0f40526644")
+        ivText.setText("0fec978d93cb56afcc7665849f900bdb")
+        mainText.setText("14P9L1h5eI8X2V7GQNdQnQeXWockfvqPJSyUZzTinSQ=")
     }
 
     fun BytesToHex(sha256HashKey : ByteArray) : String{
